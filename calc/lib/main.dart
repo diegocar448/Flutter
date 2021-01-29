d279ce9db3e0b264d7e99fe34b53dbcd67e72afb
Diego Cardoso <diegocar448@hotmail.com>
2021-01-29 16:15:15 -0300

    Calc

--- calc/lib/main.dart
+++ calc/lib/main.dart
@@ -14,20 +14,56 @@ class _MyAppState extends State<MyApp> {
     /* Campo tipo number 1*/
     TextField num1 = TextField(
       keyboardType: TextInputType.number,
+      style: TextStyle(color: Colors.deepOrange),
+      decoration: InputDecoration(
+        labelText: 'Numero 01',
+        labelStyle: TextStyle(color: Colors.deepOrange),
+        enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.deepOrange, width: 1.0)),
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.deepOrange, width: 1.0)),
+      ),
     );
     /* Campo tipo number 2*/
     TextField num2 = TextField(
       keyboardType: TextInputType.number,
+      style: TextStyle(color: Colors.deepOrange),
+      decoration: InputDecoration(
+        labelText: 'Numero 02',
+        labelStyle: TextStyle(color: Colors.deepOrange),
+        enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.deepOrange, width: 1.0)),
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.deepOrange, width: 1.0)),
+      ),
     );
 
     /* Botão */
     RaisedButton button = RaisedButton(
-      onPressed: null,
-      child: Text('Calcular'),
+        onPressed: () {},
+        child: Text('Calcular'),
+        color: Colors.deepOrange,
+        textColor: Colors.white,
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)));
+
+    /* Separador */
+    Padding separator = Padding(
+      padding: EdgeInsets.all(4.0),
     );
 
+    /* Layout do aplicativo como coluna (outra opção seria linha)  */
     Column columns = Column(
-      children: <Widget>[num1, num2, button],
+      children: <Widget>[
+        num1,
+        separator,
+        num2,
+        separator,
+        SizedBox(
+          child: button,
+          width: double.infinity,
+        ),
+      ],
     );
 
     return MaterialApp(
@@ -36,6 +72,7 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('Calculator'),
           centerTitle: true,
+          backgroundColor: Colors.deepOrange,
         ),
         body: Padding(
           padding: EdgeInsets.all(14.0),
