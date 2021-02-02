8e41937088e5248f92b6079b0264999a2ac0775a
Diego Cardoso <diegocar448@hotmail.com>
2021-02-02 17:17:51 -0300

    app_ml

--- app_ml/lib/widget/app_menu_drawer.dart
+++ app_ml/lib/widget/app_menu_drawer.dart
@@ -7,18 +7,42 @@ class AppMenuDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       children: <Widget>[
-        DrawerHeader(
-          decoration: BoxDecoration(color: Color.fromRGBO(255, 241, 89, 1)),
-          child: Container(
-              child: CircleAvatar(
-                child: Icon(Icons.people),
-                backgroundColor: Color.fromRGBO(255, 241, 89, 1),
-                foregroundColor: Colors.black,
-              ),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                  border: Border.all(color: Colors.black, width: 4.0))),
-        ),
+        Container(
+          height: 120.0,
+          child: DrawerHeader(
+            decoration: BoxDecoration(color: Color.fromRGBO(255, 241, 89, 1)),
+            child: Row(
+              children: <Widget>[
+                Container(
+                    width: 60.0,
+                    height: 60.0,
+                    child: CircleAvatar(
+                      child: Icon(Icons.people),
+                      backgroundColor: Color.fromRGBO(255, 241, 89, 1),
+                      foregroundColor: Colors.grey,
+                    ),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                        border: Border.all(color: Colors.grey, width: 4.0))),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "Olá Diego!",
+                        style: TextStyle(
+                            fontSize: 20.0, fontWeight: FontWeight.bold),
+                      ),
+                      Text("Nível Avançado"),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
       ],
     );
   }
