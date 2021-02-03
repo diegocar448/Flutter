e0ace1a44c3057488ab58d341b81fdec6388ae69
Diego Cardoso <diegocar448@hotmail.com>
2021-02-03 17:58:59 -0300

    splash

--- splash/lib/splash.dart
+++ splash/lib/splash.dart
@@ -4,11 +4,34 @@ class Splash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Image.asset('assets/images/especializati.png'),
-          CircularProgressIndicator(),
-        ],
+      body: Container(
+        width: double.maxFinite,
+        color: Colors.black,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: 250,
+              child: Image.asset('assets/images/logo-especializati.png'),
+            ),
+            Container(
+              height: 40,
+            ),
+            CircularProgressIndicator(
+                backgroundColor: Colors.white,
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.orange)),
+            Container(
+              height: 10,
+            ),
+            Text(
+              'Carregando... ',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16.0,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
