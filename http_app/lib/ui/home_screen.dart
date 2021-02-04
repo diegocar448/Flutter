9a39ce4d94edebd65635d0bd6bb220a7045b71e9
Diego Cardoso <diegocar448@hotmail.com>
2021-02-04 18:46:34 -0300

    splash

--- http_app/lib/ui/home_screen.dart
+++ http_app/lib/ui/home_screen.dart
@@ -4,19 +4,63 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      //color: Colors.black,
-      //height: 300.0,
-      width: double.maxFinite,
-      child: Padding(
-          padding: EdgeInsets.all(50.0),
-          child: Image.asset(
-            'assets/images/github.png',
-            height: 120.0,
-          )),
-      decoration: BoxDecoration(
-          color: Colors.black,
-          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(120.0))),
-    ));
+        resizeToAvoidBottomInset: false,
+        body: Column(children: <Widget>[
+          Container(
+            //color: Colors.black,
+            //height: 300.0,
+            width: double.maxFinite,
+            child: Padding(
+                padding: EdgeInsets.all(50.0),
+                child: Image.asset(
+                  'assets/images/github.png',
+                  height: 120.0,
+                )),
+            decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius:
+                    BorderRadius.only(bottomLeft: Radius.circular(120.0))),
+          ),
+          Padding(
+              padding: EdgeInsets.all(12.0),
+              child: Column(
+                children: [
+                  Container(
+                    height: 30.0,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.black, width: 1.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.black, width: 1.0),
+                        ),
+                        hintText: 'username',
+                        labelText: 'username',
+                        labelStyle: TextStyle(color: Colors.black)),
+                  ),
+                  /* Espaçamento entre botão e campo username */
+                  Container(
+                    height: 10.0,
+                  ),
+                  ButtonTheme(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0)),
+                      height: 50.0,
+                      /* largura maxima do botão double.maxFinite */
+                      minWidth: double.maxFinite,
+                      child: /* Botão */
+                          RaisedButton(
+                              textColor: Colors.white,
+                              color: Colors.black,
+                              child: Text('Buscar',
+                                  style: TextStyle(fontSize: 20.0)),
+                              onPressed: () {}))
+                ],
+              ))
+        ]));
   }
 }
