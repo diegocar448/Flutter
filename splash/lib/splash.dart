dbccf64fef7e3b3e4475cb33723a822e3978c3c9
Diego Cardoso <diegocar448@hotmail.com>
2021-02-04 09:46:44 -0300

    splash

--- splash/lib/splash.dart
+++ splash/lib/splash.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'package:splash/home.dart';
+
 class Splash extends StatefulWidget {
   Splash({Key key}) : super(key: key);
 
@@ -15,6 +17,13 @@ class _SplashState extends State<Splash> {
 
     /* Deixando a tela como fullScreen */
     SystemChrome.setEnabledSystemUIOverlays([]);
+
+    /* Após 3 segundos redirecionar para a tela Home ((qual context, newRoute(qual tela vai redirecionar))) */
+    Future.delayed(Duration(seconds: 3)).then((_) => {
+          /* com a função pushReplacacement não teremos função para voltar */
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => Home()))
+        });
   }
 
   @override
