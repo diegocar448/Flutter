96d3fbb1035fdd5440e025ec03b64068ea53af60
Diego Cardoso <diegocar448@hotmail.com>
2021-02-05 11:21:31 -0300

    http_app Model http ApiGitHub

--- http_app/lib/ui/results_screen.dart
+++ http_app/lib/ui/results_screen.dart
@@ -11,18 +11,59 @@ class ResultsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
         title: Text('Resultados'),
         centerTitle: true,
         backgroundColor: Colors.black,
       ),
       body: Center(
-        child: Text('Olá'),
+        child: FutureBuilder(
+          future: _fetchUser(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              final user = snapshot.data;
+              return Padding(
+                  padding: EdgeInsets.all(12),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      ClipRRect(
+                          borderRadius: BorderRadius.circular(100),
+                          child: Image.network(
+                            user.avatarUrl,
+                            height: 100.0,
+                          )),
+                      /* Espaçamento */
+                      Container(height: 10.0),
+                      Text(user.name,
+                          style: TextStyle(
+                              fontSize: 26.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white)),
+                      /* Espaçamento */
+                      Container(height: 10.0),
+                      Text("( ${user.blog} )",
+                          style: TextStyle(
+                              fontSize: 26.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white)),
+                    ],
+                  ));
+            } else if (snapshot.hasError) {
+              return Text('Erro: ${snapshot.error}');
+            }
+
+            return CircularProgressIndicator(
+              backgroundColor: Colors.black,
+            );
+          },
+        ),
       ),
     );
   }
 
-  Future<User> fetchUser() async {
+  Future<User> _fetchUser() async {
     final response = await http.get('https://api.github.com/users/diegocar448');
     if (response.statusCode == 200) {
       return User.fromJson(json.decode(response.body));
